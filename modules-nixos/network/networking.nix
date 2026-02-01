0a8edaaf90ad4fa3a3f52482650b1955745c6fd8
urosevicvuk <vuk23urosevic@gmail.com>
2026-02-01 13:28:18 +0100

    internet fix
    Signed-off-by: urosevicvuk <vuk23urosevic@gmail.com>

--- modules-nixos/network/networking.nix
+++ modules-nixos/network/networking.nix
@@ -14,6 +14,7 @@ in {
     networkmanager = {
       enable = true;
       wifi = {
+        backend = "iwd";
         # Disable power saving - this causes disconnects
         powersave = false;
         ## Reduce background scanning
@@ -38,15 +39,20 @@ in {
     wantedBy = ["suspend.target" "hibernate.target" "suspend-then-hibernate.target"];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.networkmanager}/bin/nmcli radio wifi off";
+      ExecStart = "${pkgs.kmod}/bin/modprobe -r mt7925e";
       ExecStartPost = [
+        "${pkgs.coreutils}/bin/sleep 3"
+        "${pkgs.kmod}/bin/modprobe mt7925e"
         "${pkgs.coreutils}/bin/sleep 2"
-        "${pkgs.networkmanager}/bin/nmcli radio wifi on"
+        "${pkgs.systemd}/bin/systemctl restart iwd.service"
+        "${pkgs.coreutils}/bin/sleep 2"
+        "${pkgs.systemd}/bin/systemctl restart NetworkManager.service"
       ];
     };
   };
-  #  services.udev.extraRules = ''
-  #    # Disable power management for WiFi
-  #    ACTION=="add", SUBSYSTEM=="net", KERNEL=="wlp*", RUN+="${pkgs.iw}/bin/iw dev $name set power_save off"
-  #  '';
+
+  services.udev.extraRules = ''
+    # Disable power management for WiFi
+    ACTION=="add", SUBSYSTEM=="net", KERNEL=="wlp*", RUN+="${pkgs.iw}/bin/iw dev $name set power_save off"
+  '';
 }
