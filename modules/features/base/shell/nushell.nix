cc3280cff7630dfc64cc891c5d3acd49d4172a2c
urosevicvuk <vuk23urosevic@gmail.com>
2026-04-20 14:17:32 +0200

    some shell improvements
    Signed-off-by: urosevicvuk <vuk23urosevic@gmail.com>

--- modules/features/base/shell/nushell.nix
+++ modules/features/base/shell/nushell.nix
@@ -1,43 +1,51 @@
-{
-  self,
-  ...
-}:
-{
-  flake.homeModules.nushell =
-    { pkgs, ... }:
-    {
-      programs.nushell = {
-        enable = true;
+{self, ...}: {
+  flake.nixosModules.nushell = {...}: {
+    home-manager.sharedModules = [self.homeModules.nushell];
+  };
 
-        settings = {
-          edit_mode = "vi";
-          cursor_shape = {
-            vi_insert = "line";
-            vi_normal = "block";
-          };
+  flake.homeModules.nushell = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    programs.nushell = {
+      enable = true;
+
+      settings = {
+        edit_mode = "vi";
+        cursor_shape = {
+          vi_insert = "line";
+          vi_normal = "block";
         };
-        #extraEnv = ''
-        #  PROMPT_INDICATOR_VI_INSERT = " ";
-        #  PROMPT_INDICATOR_VI_NORMAL = " ";
-        #'';
+      };
+      extraEnv = ''
+        $env.PROMPT_INDICATOR_VI_INSERT = ""
+        $env.PROMPT_INDICATOR_VI_NORMAL = ""
+      '';
 
-        plugins = with pkgs.nushellPlugins; [
-          polars
-          gstat
-          query
-          formats
-          #highlight
-          #skim
-          #hcl
-          #net
-          #units
-        ];
+      plugins = with pkgs.nushellPlugins; [
+        polars
+        gstat
+        query
+        formats
+      ];
+
+      shellAliases = {
+        g = "git";
+        k = "kubectl";
+        t = "talosctl";
+        rm = lib.getExe pkgs.rmtrash;
+        rmdir = lib.getExe pkgs.rmtrash;
       };
     };
 
-  flake.nixosModules.nushell =
-    { ... }:
-    {
-      home-manager.sharedModules = [ self.homeModules.nushell ];
-    };
+    home.packages = with pkgs; [
+      bat
+      television
+      ripgrep
+      tldr
+      rmtrash
+      jq
+    ];
+  };
 }
