4bf681445e09b27b7ce923f1c9d1c014aa207a25
urosevicvuk <vuk23urosevic@gmail.com>
2026-06-13 22:36:11 +0200

    flake update
    Signed-off-by: urosevicvuk <vuk23urosevic@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    nixpkgs-stable.url = "github:nixos/nixpkgs/25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/26.05";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
